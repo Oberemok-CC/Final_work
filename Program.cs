881e3c4192df1fec5747bfbdd6ebd70b5761b860
Oberemok-CC <boserf_serg@mail.ru>
2022-12-10 16:53:08 +0300

    add diagram

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+string[] InArray = new string[9] { "2012", "1984", "c#", "Star", "Wars", "Andor", ":-p", "GB", "GPB" };
+string[] OutArray = new string[InArray.Length];
+int count = 0;
+    for (int i = 0; i < InArray.Length; i++)
+       
+    {
+        
+        if (InArray[i].Length <= 3)
+        {
+            OutArray[count] = InArray[i];
+            count++;
+            
+        }
+       
+    }
+    
+    Console.Write(String.Join(" ", OutArray));
